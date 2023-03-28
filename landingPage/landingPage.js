0cae99ecc3ffe6ac931a0a7987d4ad4768624dba
Nsisong-Akpakpan <93917408+Nsisong-Akpakpan@users.noreply.github.com>
2023-03-28 14:12:14 +0100

    landing page

---
+++ landingPage/landingPage.js
@@ -0,0 +1,8 @@
+const toggleButton = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+
